d6f0aee073756aaada49638182a3dac3bd8f7d5c
max-ryzhov <max.ryzhov12@gmail.com>
2022-12-22 13:04:06 +0300

    добавил сырой тест добавление контакта

---
+++ test_add_contact.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+from selenium.webdriver.chrome.webdriver import WebDriver
+import unittest
+
+
+class AddContact(unittest.TestCase):
+    def setUp(self):
+        self.wd = WebDriver()
+        self.wd.implicitly_wait(30)
+
+    def test_add_contact(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
+        wd.find_element_by_link_text("add new").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys("Max")
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys("Ryzhov")
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys("max_ryzh")
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys("NN")
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys("MFI")
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys("+79997771122")
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys("max.ryzhov12@gmail.com")
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys("Nick")
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        wd.find_element_by_link_text("home").click()
+        wd.find_element_by_link_text("Logout").click()
+
+    def tearDown(self):
+        self.wd.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
