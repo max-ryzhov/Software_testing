28f745300218d2b5e20a4247bb744c9e8d06e546
max-ryzhov <max.ryzhov12@gmail.com>
2022-12-23 19:46:44 +0300

    перевел тест на FW pytest

--- test_add_group.py
+++ test_add_group.py
@@ -1,26 +1,23 @@
 # -*- coding: utf-8 -*-
-import unittest
+import pytest
 from group_param import GroupParam
 from application.application import Application
 
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
-
-    def test_add_group(self):
-        self.app.login(user_name="admin", password="secret")
-        self.app.create_new_group(GroupParam(group_name="Meeting", header="Hello", footer="Bye"))
-        self.app.logout()
-
-    def test_add_empty_group(self):
-        self.app.login(user_name="admin", password="secret")
-        self.app.create_new_group(GroupParam(group_name="", header="", footer=""))
-        self.app.logout()
-
-    def tearDown(self):
-        self.app.destroy()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_group(app):
+    app.login(user_name="admin", password="secret")
+    app.create_new_group(GroupParam(group_name="Meeting", header="Hello", footer="Bye"))
+    app.logout()
+
+
+def test_add_empty_group(app):
+    app.login(user_name="admin", password="secret")
+    app.create_new_group(GroupParam(group_name="", header="", footer=""))
+    app.logout()
