af8a8eb307b6e45bff4070d112dcfdaa98eab540
max-ryzhov <max.ryzhov12@gmail.com>
2022-12-27 14:30:03 +0300

    вынес заполнение формы в вспомогательный метод fill_contact_form, чтобы использовать при создании и апдейте котакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -4,7 +4,7 @@ from time import sleep
 class ContactHelper:
 
     def __init__(self, app):
-        self.app = app    # экземпляр кл ContactHelper принимает св-во - фикстуру app(экземпляр кл Application)
+        self.app = app
 
     def create(self, contact_param):
         wd = self.app.wd
@@ -12,84 +12,26 @@ class ContactHelper:
         # init creation
         wd.find_element_by_link_text("add new").click()
         # fill contact form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact_param.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact_param.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact_param.nickname)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact_param.address)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact_param.company)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact_param.mobile)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact_param.email)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact_param.middlename)
+        self.fill_contact_form(contact_param)
         # submit new creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         self.return_to_home_page()
 
-    def delete_first(self):
-        wd = self.app.wd
-    # переход на стр контактов
-        self.app.open_home_page()
-    # выбор первого контакта
-        wd.find_element_by_name('selected[]').click()
-    # submit deletion
-        wd.find_element_by_xpath("//input[@value='Delete']").click()
-    # подтверждение удаления
-        wd.switch_to.alert.accept()
-    # возврат на главную
-        self.return_to_home_page()
-
     def update_first(self, contact_param):
         wd = self.app.wd
+        self.app.open_home_page()
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
-        sleep(2)
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact_param.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact_param.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact_param.nickname)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact_param.address)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact_param.company)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact_param.mobile)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact_param.email)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact_param.middlename)
-        sleep(2)
+        self.fill_contact_form(contact_param)
         wd.find_element_by_xpath("//div[@id='content']/form/input[22]").click()
-        sleep(1)
         self.return_to_home_page()
-        sleep(5)
 
-    def return_to_home_page(self):
+    def delete_first(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        self.app.open_home_page()
+        wd.find_element_by_name('selected[]').click()
+        wd.find_element_by_xpath("//input[@value='Delete']").click()
+        wd.switch_to.alert.accept()
+        self.return_to_home_page()
 
     def fill_contact_form(self, contact_param):
         wd = self.app.wd
@@ -105,11 +47,9 @@ class ContactHelper:
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
         wd.find_element_by_name("nickname").send_keys(contact_param.nickname)
-
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
         wd.find_element_by_name("title").send_keys(contact_param.title)
-
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
         wd.find_element_by_name("address").send_keys(contact_param.address)
@@ -123,10 +63,14 @@ class ContactHelper:
         wd.find_element_by_name("email").clear()
         wd.find_element_by_name("email").send_keys(contact_param.email)
 
-        wd.find_element_by_name("bday").click()
-        wd.find_element_by_name("bday").select_by_visible_text("10")
-        wd.find_element_by_name("bmonth").click()
-        wd.find_element_by_name("bmonth").select_by_visible_text("June")
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys("1990")
+        # wd.find_element_by_name("bday").click()
+        # wd.find_element_by_name("bday").select_by_visible_text("10")
+        # wd.find_element_by_name("bmonth").click()
+        # wd.find_element_by_name("bmonth").select_by_visible_text("June")
+        # wd.find_element_by_name("byear").click()
+        # wd.find_element_by_name("byear").clear()
+        # wd.find_element_by_name("byear").send_keys("1990")
+
+    def return_to_home_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home").click()
