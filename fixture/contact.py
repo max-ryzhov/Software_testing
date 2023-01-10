fafc22aae97444a2103ce168e6ed114f7505cfc6
max-ryzhov <max.ryzhov12@gmail.com>
2023-01-10 16:25:21 +0300

    добавил проверку адреса текущей страницы перед переходами:  open_home_page  return_to_home_page  open_group_page  return_to_group_page

--- fixture/contact.py
+++ fixture/contact.py
@@ -79,7 +79,8 @@ class ContactHelper:
 
     def return_to_home_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not wd.current_url.endswith('addressbook/'):
+            wd.find_element_by_link_text("home").click()
 
     def count(self):
         wd = self.app.wd
