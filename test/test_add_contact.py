72e29b7ec5a6bdd3cf2a016a54893228d67ef636
max-ryzhov <max.ryzhov12@gmail.com>
2023-01-10 19:30:13 +0300

    для создания/модификации контактов реализовал фичу оставлять часть полей пустыми

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,10 +5,8 @@ from model.contact_param import ContactParam
 def test_add_contact(app):
     app.contact.create(ContactParam(firstname="Max", lastname="Ryzhov",  middlename="Nick", nickname="max_ryzh",
                                     address="NN", company="MFI", mobile="+79997771122", email="max.ryzhov12@gmail.com",
-                                    title='Good'))
+                                    title_user='Good'))
 
 
 def test_add_contact2(app):
-    app.contact.create(ContactParam(firstname="Петя", lastname="Иванов", middlename="Nick", nickname="pet",
-                                    address="Spb", company="Ya", mobile="+79993330011", email="pet@gmail.com",
-                                    title='Good'))
+    app.contact.create(ContactParam(firstname="Петя", lastname="Иванов", middlename="Nick", nickname="pet"))
