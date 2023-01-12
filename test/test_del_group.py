fbfb01842b47a469d25f631bce4ebed37a778245
max-ryzhov <max.ryzhov12@gmail.com>
2023-01-13 00:26:41 +0300

    1. реализованы проверки количества групп после добавления/удаления/модификации; 2. реализована сортировка групп в списке по id; 3. реализовано сравнение списка групп по их атрибутам. переопределен метод __eq__.

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -5,7 +5,19 @@ from model.group_param import GroupParam
 def test_delete_first_group(app):
     if app.group.count() == 0:
         app.group.create(GroupParam(group_name="test_delete"))
+    old_groups = app.group.get_group_list()
     app.group.delete_first()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
+    old_groups[0:1] = []    # delete elem[0]
+    assert old_groups == new_groups
+
+
+
+    
+
+
+
 
     # for i in range(10):
     #     app.group.delete_first()
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -5,7 +5,10 @@ from model.group_param import GroupParam
 def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(GroupParam(group_name="test_delete"))
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(GroupParam(group_name="New name"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_modify_group_header(app):
