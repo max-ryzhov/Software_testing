fef7011c300b0b087efd40f8ef98da0af3b04a9e
max-ryzhov <max.ryzhov12@gmail.com>
2023-01-13 14:02:49 +0300

    реализовал проверки для теста модификации групп

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,15 +3,31 @@ from model.group_param import GroupParam
 
 
 def test_modify_group_name(app):
-    if app.group.count() == 0:
-        app.group.create(GroupParam(group_name="test_delete"))
-    old_groups = app.group.get_group_list()
-    app.group.modify_first_group(GroupParam(group_name="New name"))
-    new_groups = app.group.get_group_list()
+    mod_group = GroupParam(group_name="Modified name")    # назвал модифицированную группу mod_group
+    if app.group.count() == 0:    # если групп нет, то создать
+        app.group.create(GroupParam(group_name="For modify"))
+    old_groups = app.group.get_group_list()    # сохранил список групп до модификации
+    mod_group.group_id = old_groups[0].group_id    # по умолчанию id в mod_group - None. Присваиваю id 1й группы.
+    app.group.modify_first_group(mod_group)    # модифицирую 1ю группу
+    new_groups = app.group.get_group_list()  # подтягиваю фактический список групп с веб станицы после модификации
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = mod_group    # формирую список путем замены 1й группы старого списка на mod_group
+    # сравниваем список, "какой должен быть по коду", с фактическим списком с веб страницы
+    assert sorted(old_groups, key=GroupParam.id_or_max) == sorted(new_groups, key=GroupParam.id_or_max)
 
 
-def test_modify_group_header(app):
-    if app.group.count() == 0:
-        app.group.create(GroupParam(group_name="test_delete2"))
-    app.group.modify_first_group(GroupParam(header="New header"))
+def test_modify_group_name2(app):
+    if app.group.count() == 0:    # если групп нет, то создать
+        app.group.create(GroupParam(group_name="For modify2"))
+    old_groups = app.group.get_group_list()    # сохранил список групп до модификации
+    # Сразу передаю id 1й групп в конструктор mod_group
+    mod_group = GroupParam(group_name="Second modification", group_id=old_groups[0].group_id)
+    app.group.modify_first_group(mod_group)    # модифицирую 1ю группу
+    new_groups = app.group.get_group_list()  # фактический список групп с веб станицы после модификации
+    assert len(old_groups) == len(new_groups)
+    old_groups[0] = mod_group    # формирую список путем замены 1й группы старого списка на mod_group
+    # сравниваем список, "какой должен быть по коду", с фактическим списком с веб страницы
+    assert sorted(old_groups, key=GroupParam.id_or_max) == sorted(new_groups, key=GroupParam.id_or_max)
+#
+#
+
