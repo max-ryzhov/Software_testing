5afc9c5d2c2c36826caedf4986d0fc05ac8930b8
max-ryzhov <max.ryzhov12@gmail.com>
2023-01-15 15:18:15 +0300

    кэширование и хэширования для списков контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -8,8 +8,8 @@ def test_add_contact(app):
                             title_user='Good')
     old_contacts = app.contact.get_contact_list()
     app.contact.create(added_contact)
+    assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(added_contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -7,6 +7,7 @@ def test_del_first_contact(app):
         app.contact.create(Contact(firstname="test_delete", lastname="test_delete2"))
     old_contacts = app.contact.get_contact_list()
     app.contact.delete_first()
+    assert len(old_contacts) - 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) - 1 == len(new_contacts)
     old_contacts[0:1] = []    # delete elem[0]
