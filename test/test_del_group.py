321e91b6e12ac5a5a12f40aa32c761d3e263f52e
max-ryzhov <max.ryzhov12@gmail.com>
2023-01-15 15:14:03 +0300

    кэширование и хэширования для списков групп

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -7,18 +7,7 @@ def test_delete_first_group(app):
         app.group.create(Group(group_name="test_delete"))
     old_groups = app.group.get_group_list()
     app.group.delete_first()
+    assert len(old_groups) - 1 == app.group.count()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) - 1 == len(new_groups)
     old_groups[0:1] = []    # delete elem[0]
     assert old_groups == new_groups
-
-
-
-    
-
-
-
-
-    # for i in range(10):
-    #     app.group.delete_first()
-    #     i -= 1
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -9,8 +9,9 @@ def test_modify_group_name(app):
     old_groups = app.group.get_group_list()    # сохранил список групп до модификации
     mod_group.group_id = old_groups[0].group_id    # по умолчанию id в mod_group - None. Присваиваю id 1й группы.
     app.group.modify_first_group(mod_group)    # модифицирую 1ю группу
+    assert len(old_groups) == app.group.count()    # кол-во групп не изменилось
+
     new_groups = app.group.get_group_list()  # подтягиваю фактический список групп с веб станицы после модификации
-    assert len(old_groups) == len(new_groups)
     old_groups[0] = mod_group    # формирую список путем замены 1й группы старого списка на mod_group
     # сравниваем список, "какой должен быть по коду", с фактическим списком с веб страницы
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
