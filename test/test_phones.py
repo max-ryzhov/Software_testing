5ddade3568273a75d8191221a4e14dab97112c00
max-ryzhov <max.ryzhov12@gmail.com>
2023-01-23 19:44:46 +0300

    проверка телефонов с учетом удаления не отображаемых символов

--- test/test_phones.py
+++ test/test_phones.py
@@ -1,9 +1,15 @@
+import re
 
 
 def test_phones_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_from_edit_page(0)
-    assert contact_from_home_page.homephone == contact_from_edit_page.homephone
-    assert contact_from_home_page.workphone == contact_from_edit_page.workphone
-    assert contact_from_home_page.mobilephone == contact_from_edit_page.mobilephone
+    assert contact_from_home_page.homephone == clear(contact_from_edit_page.homephone)
+    assert contact_from_home_page.workphone == clear(contact_from_edit_page.workphone)
+    assert contact_from_home_page.mobilephone == clear(contact_from_edit_page.mobilephone)
 #    assert contact_from_home_page.faxphone == contact_from_edit_page.faxphone
+
+
+def clear(s):
+    return re.sub("[() -]", "", s)  # что заменять (скобки пробел минус) /на что заменять (на пустоту)
+    # /где заменять(в строке)
