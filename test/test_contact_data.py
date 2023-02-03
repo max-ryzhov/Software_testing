195d012a1a5be3d4141c200d01aa2aa457876172
max-ryzhov <max.ryzhov12@gmail.com>
2023-02-03 18:26:51 +0300

    выделил функцию clear_spaces, удаляющую только пробелы

--- test/test_contact_data.py
+++ test/test_contact_data.py
@@ -11,29 +11,33 @@ def test_compare_view_and_edit_form(app):
     contact_from_home_page = app.contact.get_contact_list()[random_id]
     contact_from_edit_page = app.contact.get_contact_from_edit_page(random_id)
     assert contact_from_home_page.all_phones_from_hp == merge_phones_like_on_homepage(contact_from_edit_page)
-    assert contact_from_home_page.all_emails_from_hp == merge_emails_like_on_homepage(contact_from_edit_page)
+    assert clear_spaces(contact_from_home_page.all_emails_from_hp) == merge_emails_like_on_homepage(
+        contact_from_edit_page)
     # firstname/lastname добавил clear (в firstname ругается, если на edit_page пробел сзади)
-    assert clear(contact_from_home_page.firstname) == clear(contact_from_edit_page.firstname)
-    assert clear(contact_from_home_page.lastname) == clear(contact_from_edit_page.lastname)
+    assert clear_spaces(contact_from_home_page.firstname) == clear_spaces(contact_from_edit_page.firstname)
+    assert clear_spaces(contact_from_home_page.lastname) == clear_spaces(contact_from_edit_page.lastname)
     assert contact_from_home_page.address == contact_from_edit_page.address
     print()
     print(random_id, contact_from_home_page, contact_from_edit_page)
 
 
-def clear(s):
+def clear_phones(s):
     return re.sub("[() -]", "", s)  # что заменять (скобки пробел минус) /на что заменять (на пустоту)
     # /где заменять(в строке)
 
 
+def clear_spaces(s):
+    return re.sub("[ ]", "", s)
+
+
 def merge_phones_like_on_homepage(contact):
     return '\n'.join(filter(lambda x: x != "",
-                            map(lambda x: clear(x),
+                            map(lambda x: clear_phones(x),
                                 filter(lambda x: x is not None, [contact.homephone, contact.mobilephone,
-                                                               contact.workphone, contact.secondphone]))))
+                                                                 contact.workphone, contact.secondphone]))))
 
 
 def merge_emails_like_on_homepage(contact):
     return '\n'.join(filter(lambda x: x != "",
-                            map(lambda x: clear(x),
-                                filter(lambda x: x is not None, [contact.email, contact.email2,
-                                                               contact.email3]))))
+                            map(lambda x: clear_spaces(x),
+                                filter(lambda x: x is not None, [contact.email, contact.email2, contact.email3]))))
