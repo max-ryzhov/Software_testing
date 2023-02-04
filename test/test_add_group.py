c3b0f4876f3c3d5067482d6416e93dd1a3f70430
max-ryzhov <max.ryzhov12@gmail.com>
2023-02-04 23:48:56 +0300

    реализована параметризация для добавления контактов

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -8,17 +8,18 @@ import string
 # генератор случайных строк
 def random_string(prefix, maxlen):
     symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
-    phrase = "".join([random.choice(symbols) for symbol in range(random.randrange(maxlen))])
-    return prefix + phrase
+    random_str = "".join([random.choice(symbols) for symbol in range(random.randrange(maxlen))])
+    return prefix + random_str
 
 
 # генератор тестовых данных
-test_data = [Group(group_name=random_string("name_", 10), header=random_string("header_", 10), footer=random_string(
-    "footer_", 10)) for i in range(5)]
+test_data_group = [Group(group_name=random_string("name_", 10),
+                         header=random_string("header_", 10),
+                         footer=random_string("footer_", 10)) for i in range(5)]
 
 
 # название параметра, в кот. передаются данные; тестовые данные; строковое представление данных для отчета;
-@pytest.mark.parametrize('group', test_data, ids=[str(x) for x in test_data])
+@pytest.mark.parametrize('group', test_data_group, ids=[str(x) for x in test_data_group])
 def test_add_group(app, group):
     old_groups = app.group.get_group_list()
     app.group.create(group)
@@ -26,6 +27,9 @@ def test_add_group(app, group):
     new_groups = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+
+
 
     # генератор тестовых наборов
     # test_data = [Group(group_name=name, header=header, footer=footer)
