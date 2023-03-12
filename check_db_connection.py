ac9102120e1fb6c88c37876957723c28cfac21c0
max-ryzhov <max.ryzhov12@gmail.com>
2023-03-12 14:29:40 +0300

    инициализация DB данные авторизации из target.json

---
+++ check_db_connection.py
@@ -0,0 +1,29 @@
+import pymysql.cursors
+# PyMySQL - драйвер для СУБД MySQL
+
+
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='',
+                             database='addressbook',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+try:
+    cursor = connection.cursor()    # cursor - указатель на данные в базе.
+    cursor.execute('select * from group_list where group_id=410')    # cursor.execute извлечь данные
+    for row in cursor.fetchall():    # cursor.fetchall вывести данные в виде строки
+        print(row)
+finally:
+    connection.close()
+
+
+
+
+
+# with connection:
+#     with connection.cursor() as cursor:
+#
+#         # Create a new record
+#         sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
+#         cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
