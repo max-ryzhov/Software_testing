9b03fb7f75d0bf67f11f8adaa4a4830ba8d4f95c
max-ryzhov <max.ryzhov12@gmail.com>
2023-03-15 21:45:09 +0300

    ORM for groups & contacts + get_group_list get_contact_list

--- check_db_connection.py
+++ check_db_connection.py
@@ -5,7 +5,7 @@ from fixture.orm import ORMFixture
 db = ORMFixture(host='localhost', name='addressbook', user='root', password='')
 
 try:
-    lst = db.get_group_list()
+    lst = db.get_contact_list()
     for item in lst:
         print(item)
     print(len(lst))
--- fixture/orm.py
+++ fixture/orm.py
@@ -2,18 +2,20 @@ from pony.orm import *
 from datetime import datetime
 from model.group_construct import Group
 from model.contact_construct import Contact
+from pymysql.converters import decoders    # !!!!! преобзазователь типов данных для pony.orm
 
 
-# ПОНИ БУДЕТ ПЕРЕВОДИТЬ НА ЯЗЫК SQL-запросов
+# ПОНИ БУДЕТ ПЕРЕВОДИТЬ СОРЗДАННЫЙ НАМИ ORM-ЯЗЫК НА ЯЗЫК SQL-ЗАПРОСОВ
 class ORMFixture:
-    db = Database()
+    db = Database()    # объект - БД с которой работаем.
 
-    # ОПИСЫВАЕМ СТРУКТУРУ
-    # внутри класса описываем набор свойств, привязанных к таблице
-    class ORMGroup(db.Entity):  # db.Entity привязали класс к БД. этот класс описывает объекты, кот. сохраняются в БД
+    # ОПИСЫВАЕМ ORM-МОДЕЛЬ В ВИДЕ КЛАССОВ (класс по сути - описывает отдельную таблицу)
+    # внутри класса описываем набор свойств, привязанных к полям таблицы
+    class ORMGroup(db.Entity):  # db.Entity - базовый класс, вложенный в объект db.
+        # Так осуществляется привязка класса ORMGroup к db = Database()
         _table_ = 'group_list'  # название таблицы
-        group_id = PrimaryKey(int, column='group_id')  # column = "название столбца в таблице"
-        group_name = Optional(str, column='group_name')
+        group_id = PrimaryKey(int, column='group_id')    # PrimaryKey - приоритетное поле идентификации объекта
+        group_name = Optional(str, column='group_name')    # column = "название столбца в таблице"
         header = Optional(str, column='group_header')
         footer = Optional(str, column='group_footer')
 
@@ -22,23 +24,37 @@ class ORMFixture:
         id = PrimaryKey(int, column='id')
         firstname = Optional(str, column='firstname')
         lastname = Optional(str, column='lastname')
-        deprecated = Optional(datetime, column='deprecated')
+        deprecated = Optional(str, column='deprecated')    # !!! заменил тип на str, под pymysql.converters decoders
+#        deprecated = Optional(datetime, column='deprecated')
 
-    # ПРИВЯЗЫВАЕМ СВОЙСТВА К СТОЛБЦАМ БД
+    # ПРИВЯЗЫВАЕМСЯ К КОНКРЕТНОЙ БД
     def __init__(self, host, name, user, password):
-        self.db.bind('mysql', host=host, database=name, user=user, password=password)
-        self.db.generate_mapping()  # mapping как раз сопоставляет свойства описанных классов с полями таблиц
+        # bind - метод привязки объектов к БД
+        self.db.bind('mysql', host=host, database=name, user=user, password=password, conv=decoders)
+        self.db.generate_mapping()  # mapping сопоставляет свойства описанных КЛАССОВ с таблицами БД
         sql_debug(True)
 
+    # ФУНКЦИИ ПРЕОБРАЗУЮЩИЕ ОБЪЕКТЫ ORMGroup в ОБЪЕКТЫ Group
     def convert_groups_to_model(self, groups):
         def convert(group):
+            # attr.Group = attr.ORMGroup
             return Group(group_id=str(group.group_id), group_name=group.group_name, header=group.header,
                          footer=group.footer)
-
         return list(map(convert, groups))
 
-    # ФУНКЦИИ, ПОЛУЧАЮЩИЕ СПИСКИ ОБЪЕКТОВ
+    def convert_contacts_to_model(self, contacts):
+        def convert(contact):
+            # cont_id - свойство класса Contact. contact.id - свойство класса ORMContact
+            return Contact(cont_id=str(contact.id), firstname=contact.firstname, lastname=contact.lastname)
+        return list(map(convert, contacts))    # возвращает список
+
+    # ФУНКЦИИ, ПОЛУЧАЮЩИЕ СПИСКИ ОБЪЕКТОВ ИЗ БД
     @db_session  # помечаем что открывается сессия
     def get_group_list(self):
+        # делаем выборку из таблицы, но не на языке sql, а на созданном нами языке ORMFixture.ORMGroup
         return self.convert_groups_to_model(select(g for g in ORMFixture.ORMGroup))
 
+    @db_session  # помечаем что открывается сессия
+    def get_contact_list(self):
+        return self.convert_contacts_to_model(select(c for c in ORMFixture.ORMContact if c.deprecated is None))
+    # даты  deprecated преобразовываем в None
