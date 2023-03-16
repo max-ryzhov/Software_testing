a5341478f9f4b2df7fea321fa59ea90b64561c9f
max-ryzhov <max.ryzhov12@gmail.com>
2023-03-17 00:40:46 +0300

    Отношения между объектами - 3.35 get_contacts_in_group

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,11 +1,13 @@
 import pymysql.cursors
 from fixture.orm import ORMFixture
+from model.group_construct import Group
+from model.contact_construct import Contact
 
 
 db = ORMFixture(host='localhost', name='addressbook', user='root', password='')
 
 try:
-    lst = db.get_contact_list()
+    lst = db.get_contacts_in_group(Group(group_id='327'))
     for item in lst:
         print(item)
     print(len(lst))
--- fixture/orm.py
+++ fixture/orm.py
@@ -2,7 +2,7 @@ from pony.orm import *
 from datetime import datetime
 from model.group_construct import Group
 from model.contact_construct import Contact
-from pymysql.converters import decoders    # !!!!! преобзазователь типов данных для pony.orm
+# from pymysql.converters import decoders    # !!!!! преобзазователь типов данных для pony.orm
 
 
 """ПОНИ БУДЕТ ПЕРЕВОДИТЬ СОРЗДАННЫЙ НАМИ ORM-ЯЗЫК НА ЯЗЫК SQL-ЗАПРОСОВ"""
@@ -19,9 +19,7 @@ class ORMFixture:
         header = Optional(str, column='group_header')
         footer = Optional(str, column='group_footer')
         # связь контактов и групп по столбцу id таблицы address_in_groups
-#        contacts = Set(lambda: ORMFixture.ORMContact, table="address_in_groups", column='id', reverse='groups',
-#                       lazy=True)
-
+        contacts = Set(lambda: ORMFixture.ORMContact, table="address_in_groups", column='id', reverse='groups', lazy=True)
 
     class ORMContact(db.Entity):
         _table_ = 'addressbook'
@@ -31,13 +29,12 @@ class ORMFixture:
         deprecated = Optional(str, column='deprecated')    # !!! заменил тип на str, под pymysql.converters decoders
 #        deprecated = Optional(datetime, column='deprecated')
         # связь групп и контактов по столбцу id таблицы address_in_groups
-#        contacts = Set(lambda: ORMFixture.ORMGroup, table="address_in_groups", column='group_id', reverse='contacts',
-#                       lazy=True)
+        groups = Set(lambda: ORMFixture.ORMGroup, table="address_in_groups", column='group_id', reverse='contacts', lazy=True)
 
     """ПРИВЯЗЫВАЕМСЯ К КОНКРЕТНОЙ БД"""
     def __init__(self, host, name, user, password):
         # bind - метод привязки объектов к БД
-        self.db.bind('mysql', host=host, database=name, user=user, password=password, conv=decoders)
+        self.db.bind('mysql', host=host, database=name, user=user, password=password)    # , conv=decoders)
         self.db.generate_mapping()  # mapping сопоставляет свойства описанных КЛАССОВ с таблицами БД
         sql_debug(True)
 
@@ -65,3 +62,8 @@ class ORMFixture:
     def get_contact_list(self):
         return self.convert_contacts_to_model(select(c for c in ORMFixture.ORMContact if c.deprecated is None))
     # даты  deprecated преобразовываем в None
+
+    @db_session  # помечаем что открывается сессия
+    def get_contacts_in_group(self, group):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.group_id == group.group_id))[0]
+        return self.convert_contacts_to_model(orm_group.contacts)
